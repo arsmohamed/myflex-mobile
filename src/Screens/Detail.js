e1f90223d07293f562cb37eb8bacbc4f4589359e
arsmohamed <arsmohamed@outlook.com>
2024-01-20 16:10:05 -0330

    detail screen is done and now I will work on the redux to passdown the information to it

--- src/Screens/Detail.js
+++ src/Screens/Detail.js
@@ -1,63 +1,76 @@
 // ChatScreen.js
 import React from "react";
-import { View, Text, StyleSheet, ScrollView, Dimensions, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  Image,
+} from "react-native";
 import DetailHeader from "../Headers/DetailHeader";
 import HP from "../Assets/HP2.jpeg";
 import IMBD from "../Assets/IMBD.png";
 const DetailScreen = ({ route }) => {
   const { Title, img, imbd, rating } = route.params;
   return (
-    <View style={styles.COntainer_Style}>
+    <View style={styles.Container_Style}>
       <DetailHeader />
-      <View style={styles.Detail_Container_Style}>
-        {/* <ScrollView> */}
-        <Image source={HP} style={styles.Image_Style} />
-        <View style={styles.Info_Style}>
-          <Text style={[styles.Title_Style, styles.Text_color]}>{Title}</Text>
-          <View style={styles.First_Container_Style}>
-            <View style={styles.Rate_Container_Style}>
-              <Image source={IMBD} />
-              <Text style={[styles.Rating_Style, styles.Text_color]}>{rating}</Text>
+      <ScrollView>
+        <View style={styles.Detail_Container_Style}>
+          <Image source={HP} style={styles.Image_Style} />
+          <View style={styles.Info_Style}>
+            <Text style={[styles.Title_Style, styles.Text_color]}>{Title}</Text>
+            <View style={styles.First_Container_Style}>
+              <View style={styles.Rate_Container_Style}>
+                <Image source={IMBD} />
+                <Text style={[styles.Rating_Style, styles.Text_color]}>{rating}</Text>
+              </View>
+              <TouchableOpacity style={styles.button} onPress={() => console.log("add to List")}>
+                <Text style={styles.Add_Text_Style}>Add to List</Text>
+              </TouchableOpacity>
             </View>
-            <Text style={[styles.Rating_Style, styles.Text_color]}>{rating}</Text>
+            <Text style={styles.Genera_Container_Style}>
+              PG | 1h 57min | Animation, Action, Adventure | 14 December 2018 (USA)
+            </Text>
+            <Text style={styles.Cast_Container_Style}>
+              Directors: Peter Ramsey, Bob Persichetti, Rodney Rothman
+            </Text>
+            <Text style={styles.Cast_Container_Style}>
+              Cast: Shameik Moore, Jake Johnson, Hailee Steinfeld
+            </Text>
+            <Text style={styles.Description_Container_Style}>
+              Miles Morales is a New York teen struggling with school, friends and, on top of that,
+              being the new Spider-Man. When he comes across Peter Parker, the erstwhile saviour of
+              New York, in the multiverse, \Miles must train to become the new protector of his
+              city.
+            </Text>
           </View>
         </View>
-      </View>
-      {/* </ScrollView> */}
+      </ScrollView>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  COntainer_Style: {
-    flex: 0.95,
+  Container_Style: {
     backgroundColor: "black",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    // borderWidth: 1,
-    // borderColor: "red",
+    paddingBottom: "20%",
   },
   Detail_Container_Style: {
-    flex: 1,
-    width: "100%",
+    width: Dimensions.get("screen").width,
     alignItems: "center",
-    // borderWidth: 4,
-    // borderColor: "green",
+    justifyContent: "center",
   },
   Image_Style: {
     width: Dimensions.get("screen").width,
-    height: Dimensions.get("screen").height / 2,
+    height: Dimensions.get("screen").height / 1.5,
     resizeMode: "cover",
-    // borderWidth: 1,
-    // borderColor: "red",
   },
   Info_Style: {
     width: "90%",
-    height: "100%",
     paddingTop: 10,
     rowGap: 20,
-    borderWidth: 4,
-    borderColor: "red",
   },
   Title_Style: {
     fontSize: 30,
@@ -75,8 +88,38 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     columnGap: 10,
   },
+  button: {
+    width: 100,
+    height: 35,
+    backgroundColor: "#FFD900",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    borderRadius: 7,
+  },
+  Add_Text_Style: {
+    fontSize: 18,
+    alignSelf: "center",
+    justifyContent: "center",
+  },
   Rating_Style: {
-    fontSize: 25,
+    fontSize: 20,
+  },
+  Genera_Container_Style: {
+    fontSize: 16,
+    color: "#C1C1C1",
+    textAlign: "justify",
+  },
+  Cast_Container_Style: {
+    fontSize: 16,
+    textAlign: "justify",
+    color: "#828282",
+    paddingLeft: 15,
+  },
+  Description_Container_Style: {
+    fontSize: 16,
+    color: "white",
+    textAlign: "justify",
   },
   Text_color: {
     color: "white",
